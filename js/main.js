3ed1f73fd9953b0c51a255c77e29eeb138665a16
Richard Gill <richard@rgill.co.uk>
2014-11-25 22:49:07 +0000

    Added map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 $('#revolution-shortcode').revolution({
-    delay : 90000,
-    hideThumbs:10,
-    navigationVOffset : -30,
-    navigationType : "none",
-    navigationStyle : "round-old"
+  delay : 90000,
+  hideThumbs:10,
+  navigationVOffset : -30,
+  navigationType : "none",
+  navigationStyle : "round-old"
+});
+
+craftyCoffeeLocation = "17 High Street, Brompton, Kent, ME7 5AA";
+
+$(document).ready(function() {
+  $('.google-maps').gmap3({
+    map:{
+      address: craftyCoffeeLocation,
+      options:{
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.DEFAULT
+        },
+        navigationControl: true,
+        scrollwheel: false,
+        streetViewControl: false
+      }
+    },
+    marker:{
+      address: craftyCoffeeLocation
+    }
+  });
 });
